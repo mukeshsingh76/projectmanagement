acc0a4513c41a55235d920cb77c04ae8b19b1e6a
mukeshdsingh76 <mukeshdsingh76@hotmail.com>
2018-04-17 23:21:23 +0530

    Dual Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "Angular Training";
+  counterValue = 0;
+  username: string = "";
+  userlastname: string = "Singh";
+
+  increment() {
+    this.counterValue++;
+  }
 }
